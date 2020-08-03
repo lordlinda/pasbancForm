ee2fd19130968d715495fe7c474a198012ee5561
lordlinda <marialindananono@gmail.com>
2020-08-04 02:43:47 +0300

    make it better

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ if(process.env.NODE_ENV === 'production'){
 	 // app.get('*',(req,res)=>{
 	  //	res.sendFile(path.join(__dirname,'client','build','index.html'))
 	  //})
-	  app.use(express.static( 'client', 'build'))
+	 // app.use(express.static( 'client', 'build'))
 
 	 
 
