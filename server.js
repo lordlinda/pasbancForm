93daf4f3359bb98e1289f17c9255d097786732d7
lordlinda <marialindananono@gmail.com>
2020-08-04 04:01:30 +0300

    server

--- server.js
+++ server.js
@@ -40,7 +40,10 @@ app.use('/user',require('./routes/form.js'))
 //however this should only be served during deployment and not production
 if(process.env.NODE_ENV === 'production'){
 	  app.use(express.static(path.join(__dirname,'client/build')))
-	 
+	 // Handles any requests that don't match the ones above
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname,'client/build/index.html'));
+});
 
 }
 
