75bc6ff6f835c62324a1e83b5ec78690ddf36042
lordlinda <marialindananono@gmail.com>
2020-08-04 03:25:46 +0300

    make it better

---
+++ client/build/precache-manifest.77aa8f052f63ed8cf2815842101a1899.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0201ba70e8a34c5b6ac55f4f542bf132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d838624d83adac7e1a8",
+    "url": "/static/css/main.2883c533.chunk.css"
+  },
+  {
+    "revision": "6269403420a77949867b",
+    "url": "/static/js/2.103836b6.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.103836b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d838624d83adac7e1a8",
+    "url": "/static/js/main.9add0754.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
